c3ff034a070aa6b29786dbdae14dc5730aec1c6b
Tigger6208 <33379859+Tigger6208@users.noreply.github.com>
2018-10-06 14:17:31 -0500

    added gyro turn function to autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes.kt
@@ -1,10 +1,14 @@
 package org.firstinspires.ftc.teamcode
 
-import android.support.annotation.IntegerRes
+import com.qualcomm.hardware.bosch.BNO055IMU
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.hardware.DcMotor
 import com.qualcomm.robotcore.hardware.DcMotorSimple
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference
+import kotlin.math.abs
 
 @Autonomous
 class TestAuto : LinearOpMode() {
@@ -27,17 +31,42 @@ class TestAuto : LinearOpMode() {
         }
     }
 
+    val imu: BNO055IMU by lazy {
+        hardwareMap.get(BNO055IMU::class.java, "imu").apply {
+            initialize(BNO055IMU.Parameters())
+        }
+    }
+
+    val heading: Double
+        get() = imu.getAngularOrientation(
+                AxesReference.INTRINSIC,
+                AxesOrder.ZYX,
+                AngleUnit.DEGREES
+        ).firstAngle.toDouble()
+
+    fun BNO055IMU.waitForGyoCalibraion() {
+       telemetry.addLine("calibrate gyro")
+        telemetry.update()
+        while (isGyroCalibrated.not()) {
+            idle()
+        }
+        telemetry.update()
+    }
+
     override fun runOpMode() {
         waitForStart()
+        imu.waitForGyoCalibraion()
+        turn(0.5, 90.0)
+        turn(0.5,-90.0)
 
-        drive(0.5, 2000)
+        /*drive(0.5, 2000)
         sleep(1000)
         drive(-0.5, -400)
         leftMotor.power = 0.3
         rightMotor.power = -0.3
         sleep(1250)
         rightMotor.power = 0.3
-        drive(0.5, 5000)
+        drive(0.5, 5000)*/
     }
 
     fun drive(power: Double, distance: Int) {
@@ -47,38 +76,49 @@ class TestAuto : LinearOpMode() {
         leftMotor.power = power
         when {
             distance > 0 -> while (
-                leftMotor.currentPosition < initialLeftMotorPosition + distance &&
-                rightMotor.currentPosition < initialRightMotorPosition + distance &&
-                opModeIsActive()
+                    leftMotor.currentPosition < initialLeftMotorPosition + distance &&
+                    rightMotor.currentPosition < initialRightMotorPosition + distance &&
+                    opModeIsActive()
             ) {
                 idle()
             }
             distance < 0 -> while (
-                leftMotor.currentPosition > initialLeftMotorPosition + distance &&
-                rightMotor.currentPosition > initialRightMotorPosition + distance &&
-                opModeIsActive()
+                    leftMotor.currentPosition > initialLeftMotorPosition + distance &&
+                    rightMotor.currentPosition > initialRightMotorPosition + distance &&
+                    opModeIsActive()
             ) {
                 idle()
             }
             else -> return
         }
-
-        leftMotor.power = 0.0
-        rightMotor.power = 0.0
     }
 
-  /*  fun gyroTurn(heading: Double) {
-        var gyroSensor =
-       Assume start heading is 315.
-       We know target heading from function call.
-       Determine we need to turn right or left.
-       Determine speed of motors.
-       Turn the robot.
-       Stop the turn at the perfect degree.
 
-         225 degrees for depot.
-                           X = degrees we want to go to.
-                           4 numbers. 45, 135, 225, 315. *\
 
+        fun turn(power: Double, targetHeading: Double) {
+            val initialHeading = heading
+            when {
+                initialHeading > targetHeading -> {
+                    leftMotor.power = abs(power)
+                    rightMotor.power = -abs(power)
+                    while(heading > targetHeading){
+                        telemetry.addLine("Heading: $heading")
+                        telemetry.update()
+                        idle()
+                    }
+
+                }
+                initialHeading < targetHeading -> {
+                    leftMotor.power = -abs(power)
+                    rightMotor.power = abs(power)
+                    while(heading < targetHeading) {
+                        telemetry.addLine("Heading: $heading")
+                        telemetry.update()
+                        idle()
+                    }
+                }
+            }
+            leftMotor.power = 0.0
+            rightMotor.power = 0.0
+        }
     }
-}
