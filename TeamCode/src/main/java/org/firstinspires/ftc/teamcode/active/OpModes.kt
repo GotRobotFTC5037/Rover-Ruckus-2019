3f06a0f5e512026a9da2d20346de41efc2f485f4
Raymond Burrage <ray.inc.05@gmail.com>
2018-10-12 20:39:55 -0500

    Can get the right gold ore

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/active/OpModes.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/active/OpModes.kt
@@ -3,10 +3,7 @@ package org.firstinspires.ftc.teamcode.active
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import com.qualcomm.robotcore.eventloop.opmode.Disabled
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
-import org.firstinspires.ftc.teamcode.lib.IMULocalizer
-import org.firstinspires.ftc.teamcode.lib.RobotMoveAction
-import org.firstinspires.ftc.teamcode.lib.RobotTankDriveTrain
-import org.firstinspires.ftc.teamcode.lib.createRobot
+import org.firstinspires.ftc.teamcode.lib.*
 
 @Autonomous
 class LibAutonomous : LinearOpMode() {
@@ -20,10 +17,24 @@ class LibAutonomous : LinearOpMode() {
             }
         }
         robot.setupAndWaitForStart()
-        robot.runAction(RobotMoveAction.timeDrive(1500L,0.5))
-        robot.runAction(RobotMoveAction.timeDrive(500L,-0.5))
-        robot.runAction(RobotMoveAction.timeTurn(900,-0.4))
-        robot.runAction(RobotMoveAction.timeDrive(3500,1.0))
+        robot.runAction(RobotMoveAction.timeTurn(400,0.3))
+        robot.runAction(RobotMoveAction.timeDrive(875,0.45))
+        robot.runAction(RobotMoveAction.timeDrive(100,0.0))
+        robot.runAction(RobotMoveAction.timeTurn(300,-0.4))
+        robot.runAction(RobotMoveAction.timeDrive(650,0.5))
+        robot.runAction(RobotMoveAction.timeDrive(550,-0.7))
+        robot.runAction(RobotMoveAction.timeTurn(550, -0.5))
+        robot.runAction(RobotMoveAction.timeDrive(100,0.0))
+        robot.runAction(RobotMoveAction.timeDrive(3500, 0.7))
+
+
+
+
+        //robot.runAction(RobotMoveAction.timeDrive(1500L,0.5))
+        //robot.runAction(RobotMoveAction.timeDrive(500L,-0.5))
+        //robot.runAction(RobotMoveAction.timeTurn(900,-0.4))
+        //robot.runAction(RobotMoveAction.timeDrive(3750,1.0))
     }
 
 }
+
