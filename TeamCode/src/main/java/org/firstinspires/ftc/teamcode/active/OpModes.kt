ae50fcc84abeda5fafda0ce84d64a2231be52b7a
Patrick Stephen Jr <pjtnt11@gmail.com>
2018-10-13 16:16:06 -0500

    Fixed some formatting.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/active/OpModes.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/active/OpModes.kt
@@ -3,7 +3,10 @@ package org.firstinspires.ftc.teamcode.active
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.hardware.AnalogInput
-import org.firstinspires.ftc.teamcode.lib.*
+import org.firstinspires.ftc.teamcode.lib.Robot
+import org.firstinspires.ftc.teamcode.lib.RobotMoveAction
+import org.firstinspires.ftc.teamcode.lib.RobotTankDriveTrain
+import org.firstinspires.ftc.teamcode.lib.createRobot
 
 @Autonomous
 class LibAutonomous : LinearOpMode() {
@@ -27,9 +30,11 @@ class LibAutonomous : LinearOpMode() {
             potentiometer.voltage < 1.1 -> {
                 left(robot)
             }
+
             potentiometer.voltage > 2.2 -> {
                 right(robot)
             }
+
             potentiometer.voltage > 1.1 && potentiometer.voltage < 2.2 -> {
                 center(robot)
             }
@@ -38,39 +43,39 @@ class LibAutonomous : LinearOpMode() {
     }
 
     fun left(robot: Robot) {
-        robot.runAction(RobotMoveAction.timeTurn(425,-0.3))
+        robot.runAction(RobotMoveAction.timeTurn(425, -0.3))
         sleep(100)
-        robot.runAction(RobotMoveAction.timeDrive(650,0.5))
+        robot.runAction(RobotMoveAction.timeDrive(650, 0.5))
         sleep(100)
-        robot.runAction(RobotMoveAction.timeTurn(325,0.45))
+        robot.runAction(RobotMoveAction.timeTurn(325, 0.45))
         sleep(100)
-        robot.runAction(RobotMoveAction.timeDrive(550,0.5))
+        robot.runAction(RobotMoveAction.timeDrive(550, 0.5))
         sleep(50)
-        robot.runAction(RobotMoveAction.timeDrive(500,-0.2))
+        robot.runAction(RobotMoveAction.timeDrive(500, -0.2))
         sleep(100)
-        robot.runAction(RobotMoveAction.timeTurn(225,0.4))
+        robot.runAction(RobotMoveAction.timeTurn(225, 0.4))
         sleep(100)
-        robot.runAction(RobotMoveAction.timeDrive(700,-0.5))
+        robot.runAction(RobotMoveAction.timeDrive(700, -0.5))
     }
 
     fun center(robot: Robot) {
-        robot.runAction(RobotMoveAction.timeDrive(1500L,0.5))
-        robot.runAction(RobotMoveAction.timeDrive(500L,-0.5))
-        robot.runAction(RobotMoveAction.timeTurn(900,-0.4))
-        robot.runAction(RobotMoveAction.timeDrive(500,1.0))
+        robot.runAction(RobotMoveAction.timeDrive(1500L, 0.5))
+        robot.runAction(RobotMoveAction.timeDrive(500L, -0.5))
+        robot.runAction(RobotMoveAction.timeTurn(900, -0.4))
+        robot.runAction(RobotMoveAction.timeDrive(500, 1.0))
     }
 
     fun right(robot: Robot) {
-        robot.runAction(RobotMoveAction.timeTurn(400,0.3))
-        robot.runAction(RobotMoveAction.timeDrive(875,0.45))
+        robot.runAction(RobotMoveAction.timeTurn(400, 0.3))
+        robot.runAction(RobotMoveAction.timeDrive(875, 0.45))
         sleep(100)
-        robot.runAction(RobotMoveAction.timeTurn(300,-0.4))
-        robot.runAction(RobotMoveAction.timeDrive(650,0.5))
+        robot.runAction(RobotMoveAction.timeTurn(300, -0.4))
+        robot.runAction(RobotMoveAction.timeDrive(650, 0.5))
         robot.runAction(RobotMoveAction.timeTurn(550, -0.5))
         sleep(100)
         robot.runAction(RobotMoveAction.timeDrive(50, 0.7))
-        robot.runAction(RobotMoveAction.timeTurn(100,-0.35))
-        robot.runAction(RobotMoveAction.timeDrive(500,0.7))
+        robot.runAction(RobotMoveAction.timeTurn(100, -0.35))
+        robot.runAction(RobotMoveAction.timeDrive(500, 0.7))
     }
 
 }
