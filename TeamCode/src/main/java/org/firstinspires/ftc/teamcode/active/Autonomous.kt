92d40269f908a8a354dc08a41c6fcff2d7c3b09a
Raymond Burrage <ray.inc.05@gmail.com>
2018-11-06 20:48:40 -0600

    Everything we need/important

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/active/Autonomous.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/active/Autonomous.kt
@@ -48,41 +48,41 @@ class GoldPositionNotDetectedException : RuntimeException("The gold position was
 class DepotAutonomous : LinearOpMode() {
 
     private val leftAction = actionSequenceOf(
+        drive(90,0.4),
         turnTo(20.0, 1.0) then wait(100),
         drive(1150, 0.4),
         turnTo(-20.0, 1.0) then wait(100),
         drive(820, 0.4),
         deliverMarkerAction,
-        drive(-180, 0.3),
+        drive(-180, 0.4),
         turnTo(-80.0, 1.0) then wait(100),
-        drive(710, 0.2),
+        drive(720, 0.4),
         turnTo(-120.0, 1.0) then wait(100),
-        drive(360, 0.5)
+        drive(1500, 0.5)
     )
 
     private val centerAction = actionSequenceOf(
         drive(1900, 0.4),
-        drive(-650, 0.4),
+        drive(-85, 0.4),
         turnTo(-90.0, 1.0),
         deliverMarkerAction,
-        drive(-500, 0.4),
-        turn(-90.0, 0.4),
-        drive(1000, 0.4),
-        turn(-40.0, 0.4),
+        drive(720, 0.4),
+        turnTo(-110.0, 1.0),
         drive(1500, 0.7)
     )
 
     private val rightAction = actionSequenceOf(
+       drive(90,0.4),
         turnTo(-20.0, 1.0) then wait(100),
         drive(1150, 0.4),
         turnTo(20.0, 1.0) then wait(100),
         drive(820, 0.4),
         deliverMarkerAction,
-        drive(-180, 0.3),
-        turnTo(80.0, 1.0) then wait(100),
-        drive(710, 0.2),
-        turnTo(120.0, 1.0) then wait(100),
-        drive(360, 0.5)
+        drive(-180, 0.4),
+        turnTo(-90.0, 1.0) then wait(100),
+        drive(725, 0.4),
+        turnTo(-110.0, 1.0) then wait(100),
+        drive(1500, 0.5)
     )
 
     @Throws(InterruptedException::class)
@@ -99,28 +99,42 @@ class DepotAutonomous : LinearOpMode() {
 class CraterAutonomous : LinearOpMode() {
 
     private val leftAction = actionSequenceOf(
-        turnTo(-20.0, 1.0) then wait(100),
-        drive(1150, 0.4),
-        turnTo(20.0, 1.0) then wait(100),
-        drive(820, 0.4) then wait(1000)
+        drive(5,0.4),
+        turnTo(25.0, 1.0),
+        drive(750, 0.4),
+        drive(-100,0.4),
+        turnTo(75.0, 1.0),
+        drive(820, 0.4),
+        turnTo(110.0,1.0),
+        drive(800,0.5),
+        deliverMarkerAction,
+        drive(-1500,0.7)
+
     )
 
     private val centerAction = actionSequenceOf(
-        drive(420, 0.6),
-        drive(-180,0.3),
-        turnTo(90.0,1.0),
-        drive(720,0.4),
-        turnTo(110.0,1.0),
-        drive(540,0.4) then wait(2000),
+        drive(550, 0.6).apply { timeoutMillis = 1000 },
+        drive(-80,0.3),
+        turnTo(72.5,1.0),
+        drive(900,0.4),
+        turnTo(115.0,1.0),
+        drive(600,0.4),
+        deliverMarkerAction,
         drive(-2000,0.7)
 
     )
 
     private val rightAction = actionSequenceOf(
-        turnTo(20.0, 1.0) then wait(100),
-        drive(1150, 0.4),
-        turnTo(-20.0, 1.0) then wait(100),
-        drive(820, 0.4) then wait(1000)
+        drive(5,0.4),
+        turnTo(-25.0,1.0),
+        drive(750,0.4),
+        drive(-100,0.4),
+        turnTo(75.0,1.0),
+        drive(1000,0.4),
+        turnTo(110.0,1.0),
+        drive(1800,0.5),
+        deliverMarkerAction,
+        drive(-1500,0.7)
     )
 
     @Throws(InterruptedException::class)
